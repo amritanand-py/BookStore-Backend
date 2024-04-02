bcbf8a5c6b5d0e79e60ad9b32a09c99bae7e39da
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-04-02 17:07:39 +0530

    Add; GetAll Books Complete

--- CommonLayer/obj/Debug/net8.0/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Debug/net8.0/CommonLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78dbe92d383582ecd26606a37d33d34e76f2ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0039191b883e074451721561cf43ea577be87946")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
